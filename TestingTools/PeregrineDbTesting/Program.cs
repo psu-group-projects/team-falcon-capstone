d1be08981db6ac157a7b84f4f9d7864d19ba8c96
Nicholas Benson <nickgbenson@gmail.com>
2011-12-12 18:39:37 -0800

    Continued working on DB testing tool.

--- TestingTools/PeregrineDbTesting/Program.cs
+++ TestingTools/PeregrineDbTesting/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Data.SqlClient;
+
 namespace PeregrineDbTesting
 {
     class Program
@@ -17,6 +19,11 @@ namespace PeregrineDbTesting
                     case "c":
                         Console.WriteLine("Creating a new PeregrineDB will completely overwrite your current PeregrineDB.");
                         Console.WriteLine("Are you sure you wish to do this?");
+
+                        SqlConnection connection = new SqlConnection("connectionString");
+                        SqlCommand command = new SqlCommand("script",connection);
+                        command.ExecuteNonQuery();
+
                         break;
                     default:
                         Console.WriteLine("Unknown argument: {0}", arg);
