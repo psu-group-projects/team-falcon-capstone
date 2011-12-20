efeeebabc3ac46f173a867d9e9856189b9db4f5f
Nicholas Benson <nickgbenson@gmail.com>
2011-12-19 17:26:53 -0800

    DB tool now creates the DB and inserts the tables on a local server

--- TestingTools/PeregrineDbTesting/Program.cs
+++ TestingTools/PeregrineDbTesting/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using System.Data.SqlClient;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace PeregrineDbTesting
 {
@@ -31,7 +32,6 @@ namespace PeregrineDbTesting
         {
             FileInfo file;
             string script;
-            SqlCommand command;
 
             Console.WriteLine("Creating a new PeregrineDB will completely overwrite your current PeregrineDB (if it exists).");
             Console.WriteLine("Are you sure you wish to do this?");
@@ -47,10 +47,46 @@ namespace PeregrineDbTesting
 
             file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreatePeregrineDB.sql");
             script = file.OpenText().ReadToEnd();
-            command = new SqlCommand(script, connection);
-            command.ExecuteNonQuery();
+            executeSqlStrings(connection, script);
 
+            file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateMessagesTable.sql");
+            script = file.OpenText().ReadToEnd();
+            executeSqlStrings(connection, script);
+
+            file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateProcessTable.sql");
+            script = file.OpenText().ReadToEnd();
+            executeSqlStrings(connection, script);
+            
+            file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateJobTable.sql");
+            script = file.OpenText().ReadToEnd();
+            executeSqlStrings(connection, script);
+
+            file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateLogRelTable.sql");
+            script = file.OpenText().ReadToEnd();
+            executeSqlStrings(connection, script);
+            
             connection.Close();
         }
+
+        // Commands can only be one batch. Batches are seperated by GO statements.
+        static string[] splitSqlAtGo(string commandString)
+        {
+            string[] commands = Regex.Split(commandString, "GO\r\n");
+            return commands;
+        }
+
+        static void executeSqlStrings(SqlConnection connection, string script)
+        {
+            string[] commandtexts = splitSqlAtGo(script);
+
+            foreach (string commandtext in commandtexts)
+            {
+                //Console.WriteLine("Splt--------------");
+                //Console.WriteLine(commandtext);
+                SqlCommand command = new SqlCommand(commandtext, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+
     }
 }
