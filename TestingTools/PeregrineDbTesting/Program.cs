f18398733b339eb1fdb5a291844949415e31da4f
Nicholas Benson <nickgbenson@gmail.com>
2011-12-28 14:59:10 -0800

    Can now specify db name in DB creation tool.

--- TestingTools/PeregrineDbTesting/Program.cs
+++ TestingTools/PeregrineDbTesting/Program.cs
@@ -38,13 +38,17 @@ namespace PeregrineDbTesting
             FileInfo file;
             string script;
             string reply;
+            string oldDBName = "PeregrineDB";
+            string dbName = "TestDB";
 
-            Console.WriteLine("Creating a new PeregrineDB will completely overwrite your current PeregrineDB (if it exists).");
-            Console.WriteLine("Are you sure you wish to do this? (yes/no)");
-
+            Console.Write("Enter a database name (Enter for {0}): ", dbName);
             reply = Console.ReadLine();
+            if (reply != "" && reply != "master") dbName = reply;            
 
-            if (reply == "yes")
+            Console.WriteLine("Warning! Creating a new {0} database will completely overwrite any other database of the same name.", dbName);
+            Console.Write("Are you sure you wish to do this (yes/no)? ");
+            reply = Console.ReadLine();
+            if (reply.ToLower() == "yes")
             {
 
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
@@ -57,28 +61,33 @@ namespace PeregrineDbTesting
 
                 connection.Open();
 
-                Console.WriteLine("Creating database...");
+                Console.WriteLine("Creating {0} database...", dbName);
 
                 file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreatePeregrineDB.sql");
                 script = file.OpenText().ReadToEnd();
+                script = script.Replace(oldDBName, dbName);
                 executeSqlStrings(connection, script);
 
                 Console.WriteLine("Creating tables...");
 
                 file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateMessagesTable.sql");
                 script = file.OpenText().ReadToEnd();
+                script = script.Replace(oldDBName, dbName);
                 executeSqlStrings(connection, script);
 
                 file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateProcessTable.sql");
                 script = file.OpenText().ReadToEnd();
+                script = script.Replace(oldDBName, dbName);
                 executeSqlStrings(connection, script);
 
                 file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateJobTable.sql");
                 script = file.OpenText().ReadToEnd();
+                script = script.Replace(oldDBName, dbName);
                 executeSqlStrings(connection, script);
 
                 file = new FileInfo("C:\\falcon\\source\\TestingTools\\PeregrineDbTesting\\SQL\\DropCreateLogRelTable.sql");
                 script = file.OpenText().ReadToEnd();
+                script = script.Replace(oldDBName, dbName);
                 executeSqlStrings(connection, script);
 
                 Console.WriteLine("Closing connection to SQL server...");
