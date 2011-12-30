ab45cdfc66f4bc04cd1dbf054c5cd868383c61d4
Firstname Lastname <ntanh1989@gmail.com>
2011-12-30 13:51:25 -0800

    3rd commit

---
+++ PeregrineWebUI/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeregrineWebUI.Models
+{
+    public class Message
+    {
+        public Message() { }
+
+        public int ID { get; set; }
+        public string content { get; set; }
+        public string processName { get; set; }
+        public int priority { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ PeregrineWebUI/VisualStudioCode_Experiment/JSON_MessagePage/TestModel/TestModel/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestModel.Models;
+using TestModel.Models.Repository;
+
+namespace TestModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Message/
+
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+
+        // data GET service
+        public JsonResult getMessage()
+        {
+              var pageData = MessageRepository.GetMessageDatas(1, 1, "", 5);
+              var messages = pageData.Data;
+ 
+              return Json(messages,JsonRequestBehavior.AllowGet);
+        }
+
+        
+    }
+}
