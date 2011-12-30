407220053fcd3e1a5eee2a63c9f92a48596481ab
Wilson Lu <luwwww@gmail.com>
2011-12-29 16:07:54 -0800

    llhhhi:# with '#' will be ignored, and an empty message aborts the commit. Test custom stored procedure, database connection, object relational mapping

---
+++ PeregrineDB/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+
+namespace ParegrineDB
+{
+    /*
+    [Table(Name = "Process")]
+    public class Process
+    {
+        private int _ProcessID;
+        [Column(IsPrimaryKey = true, Storage = "_ProcessID")]
+        public int ProcessID
+        {
+            get
+            {
+                return this._ProcessID;
+
+            }
+
+            set
+            {
+                this._ProcessID = value;
+            }
+        }
+
+
+        private string _ProcessName;
+        [Column(Storage = "_ProcessName")]
+        public string ProcessName
+        {
+            get
+            {
+                return this._ProcessName;
+
+            }
+
+            set
+            {
+                this._ProcessName = value;
+            }
+        }
+
+
+        private int _State;
+        [Column(Storage = "_State")]
+        public int State
+        {
+            get
+            {
+                return this._State;
+
+            }
+
+            set
+            {
+                this._State = value;
+            }
+        }
+
+    }
+
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Connection String
+            PeregrineDBDataContext db = new PeregrineDBDataContext(@"Data Source=CAPSTONEBB;Initial Catalog=PeregrineDB;Integrated Security=True");
+
+            //Get a typed table to run query
+            var processes = db.GetTable1();
+
+            //Attach log to sthow generated SQL
+            db.Log = Console.Out;
+
+            // Query all processes
+            var processQuery =
+                from Pro in processes
+                select Pro;
+
+            // Display Query result
+            
+            foreach (Process pro in processQuery)
+            {
+                Console.WriteLine("ProcessID = {0} ProcessName = {1} State = {2}", pro.ProcessID, pro.ProcessName, pro.State);
+            }
+
+            // Prevent from closing the console
+            Console.ReadLine();
+                
+        }
+    }
+}
---
+++ PeregrineDB_WinForm/PeregrineDB_WinForm/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+
+namespace PeregrineDB_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            processBindingSource.DataSource
+                = peregrineDB_formDataContext1.Processes;
+
+  
+            using (SqlConnection c = new SqlConnection(
+            Properties.Settings.Default.PeregrineDBConnectionString))
+            {
+                c.Open();
+                // 2
+                // Create new DataAdapter
+                using (SqlDataAdapter a = new SqlDataAdapter("SELECT * FROM Process", c))
+                {
+                    // 3
+                    // Use DataAdapter to fill DataTable
+                    DataTable t = new DataTable();
+                    a.Fill(t);
+
+                    // 4
+                    // Render data onto the screen
+                    // dataGridView1.DataSource = t; // <-- From your designer
+                }
+                c.Close();
+            }
+
+
+
+  
+
+        }
+      
+        private PeregrineDB_formDataContext peregrineDB_formDataContext1
+            = new PeregrineDB_formDataContext();
+
+        private void processBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            peregrineDB_formDataContext1.UpdateProcesses(2,"UpdateProcess2",1);
+            peregrineDB_formDataContext1.SubmitChanges();
+        }
+
+        private void processDataGridView_CellContentClick(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        /*
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+
+            var results = from pro in peregrineDB_formDataContext1.Processes
+                          select pro;
+
+            foreach(Process pro in results)
+            {
+                processDataGridView.Rows.Add(pro);
+            }
+
+
+
+
+        }
+
+        */
+        
+
+    }
+}
+
