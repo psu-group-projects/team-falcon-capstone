9c8493ff52da6b35e2bf30a6b3d6fa7d225c74c9
Firstname Lastname <ntanh1989@gmail.com>
2012-01-01 12:15:15 -0800

    Fixed a bug. Changed pageSize to 10

---
+++ PeregrineUI_2/Scripts/Helper/Peregrine.Helper.js
@@ -0,0 +1,36 @@
+﻿/*
+Author   : Chinh T Cao
+Version  : 1.0.0
+Date     : 12/29/2011
+Copyright: Capstone Project Team Falcon 2011 All right reserved
+*/
+
+function paging (page) {
+    $.ajax({      
+        url: '/Home/ProcessList',
+        data: { "page": page },
+        success: function (data) {
+            $('div.process-list').empty();
+            $('div.process-list').append(data);
+        },
+        error: function (result) {
+            alert(result);
+        }
+    });
+}
+
+function searching(x) {
+    var searchval = document.getElementById(x).value;
+
+    $.ajax({
+        url: '/Home/AjaxSearch',
+        data: { "searchpattern": searchval },
+        success: function (data) {
+            $('div.process-list').empty();
+            $('div.process-list').append(data);
+        },
+        error: function (result) {
+            alert(result);
+        }
+    });
+}
