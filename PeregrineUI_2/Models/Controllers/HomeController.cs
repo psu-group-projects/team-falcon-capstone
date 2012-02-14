c6e0e725946d03f5360556fc063672aa5291547d
Chinh Cao <ccao@pdx.edu>
2012-02-14 14:34:22 -0800

    CC : fixed get SU_SD message checkbox, implemented websettings

---
+++ PeregrineUI_2/Models/Controllers/HomeController.cs
@@ -0,0 +1,232 @@
+﻿/*
+Author   : Chinh T Cao
+Version  : 1.0.0
+Date     : 12/29/2011
+Copyright: Capstone Project Team Falcon 2011 All right reserved
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PeregrineUI_2.Models;
+using PeregrineUI_2.Models.Repository;
+using PeregrineUI_2.Properties;
+
+namespace PeregrineUI_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // Specify how many entries for one page
+        // Will be added into config file in the future
+        //
+        private int PageSize = 5;
+
+        public HomeController()
+        {
+            Settings Web_setting = new Settings();
+            PageSize = Web_setting.Refresh_Rate;
+        }
+        
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        /*
+        [HttpPost]
+        public ActionResult MainPageAjaxUpdate(int page, int sort_input, string SearchPattern)
+        {
+            int sort_columm = sort_input / 2;
+            int sort_type = sort_input % 2;  // sort_type = [0 for accending and 1 for descending]
+
+            var pagingContext = SummaryRepository.GetSummaryDataByPage(page, sort_columm, sort_type, SearchPattern, PageSize);
+            return PartialView("ProcessList", pagingContext);
+        }*/
+
+        [HttpPost]
+        public ActionResult MainPageAjaxUpdate(int page, int sort_input, string SearchPattern)
+        {
+            int sort_columm = sort_input / 2;
+            int sort_type = sort_input % 2;  // sort_type = [0 for accending and 1 for descending]
+
+            var pagingContext = SummaryRepository.GetSummaryDataByPage(page, sort_columm, sort_type, SearchPattern, PageSize);
+            return PartialView("ProcessList", pagingContext);
+        }
+
+        [HttpPost]
+        public ActionResult ProcessMsgUpdate(int page, string processName)
+        {
+            var pagingContext = MessageRepository.GetMessageByProcess(page, PageSize, processName);
+            return PartialView("Message", pagingContext);
+        }
+
+        [HttpPost]
+        public ActionResult ProcessJobUpdate(int page, string processName)
+        {
+            var pagingContext = JobRepository.GetJobByProcess(page, PageSize, processName);
+            return PartialView("Job", pagingContext);
+        }
+
+        [HttpGet]
+        public ViewResult MsgInquiry()
+        {  
+            return View();
+        }
+
+        // Message inquriy page
+        [HttpPost]
+        public ActionResult MsgInquiryUpdate(   string page_number,
+                                                string sort_option,
+                                                string msg_priority,
+                                                string process_name, 
+                                                string SU_SD_msg)
+        {
+            int input_msg_priority;
+
+            // Parsing process
+            if (msg_priority == "")
+                input_msg_priority = 0;
+            else
+                input_msg_priority = Convert.ToInt32(msg_priority);
+
+            // Parsing the sort_option info
+            int sort_option_input = Convert.ToInt32(sort_option);
+            int sort_columm = sort_option_input / 2;
+            int sort_type = sort_option_input % 2;  // sort_type = [0 for accending and 1 for descending]
+
+            var pagingContext = MsgInquiryRepo.GetMessages( Convert.ToInt32(page_number),
+                                                            sort_columm,
+                                                            sort_type,
+                                                            input_msg_priority,
+                                                            process_name,
+                                                            Convert.ToInt32(SU_SD_msg),
+                                                            PageSize);
+
+            return PartialView("MessageList", pagingContext);
+        }
+
+        [HttpPost]
+        public string MsgInq_getfulldetail(string msg_id)
+        {
+            string result = "asdfsdfsdfsdfsdfsafdsdfsdfsdfsdfsdfsdf" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsldkfjlsdjflskjdf" +
+                            " asfjlsdjflaksjflksjdfljsdlfkjsldkfjlsd" +
+                            " askdjflskdjflkjsdlkfjsl222222222222222";
+            return result;
+        }
+    }
+}
+
+//padding: 3px 18px 3px 10px;
