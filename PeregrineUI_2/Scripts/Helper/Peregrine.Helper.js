db7bd4c7078411cf7a7d24de52e50dfe7bc98397
Firstname Lastname <ntanh1989@gmail.com>
2012-02-14 17:55:19 -0800

    Done: Get info from processName in Msg page

--- PeregrineUI_2/Scripts/Helper/Peregrine.Helper.js
+++ PeregrineUI_2/Scripts/Helper/Peregrine.Helper.js
@@ -298,3 +298,23 @@ function Msg_inquiry_sorting(chkboxname) {
 
     MsgInquiryUpdate(msg_inquiry_current_page, s_option, document.getElementById('process_prio_input').value, document.getElementById('process_name_input').value, (document.getElementById('SU_SD_Checkbox').checked + 0))
 }
+
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
+}
